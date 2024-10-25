123daf0f45caec22b4c95e065063f42d523f229c
W Q <78559371+WalidQasas@users.noreply.github.com>
2024-10-25 13:57:45 +0300

    Image Attempt 3

--- src/components/usercard.jsx
+++ src/components/usercard.jsx
@@ -1,10 +1,11 @@
 import styles from './components.module.css';
 import { Button } from './button';
+import myImage from 'src\assets\walid.jpg';
 
 export function UserCard() {
   return (
     <div className={styles.card}>
-      <img src='./src/assets/walid.jpg' className={styles.card__avatar} />
+      <img src={myImage} className={styles.card__avatar} />
       <div className={styles.card__body}>
         <h3 className={styles.card__title}>Mohamad Qamhieh</h3>
         <p className={styles.card__subtitle}>Frontend Engineer</p>
