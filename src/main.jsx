2ab13dab5e3aa195067d73e57ab4cd4fe2d72e49
W Q <78559371+WalidQasas@users.noreply.github.com>
2024-10-25 12:30:19 +0300

    Inital cleaning / renaming

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import Root from './routes/root.jsx';
-import ErrorPage from './error-page.jsx';
-import Contact from './routes/contact.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
