b22279edacb2fbda03b9b394a06ae1b187d1f643
W Q <78559371+WalidQasas@users.noreply.github.com>
2024-10-25 13:41:50 +0300

    Update usercard.jsx

--- src/components/usercard.jsx
+++ src/components/usercard.jsx
@@ -4,7 +4,7 @@ import { Button } from './button';
 export function UserCard() {
   return (
     <div className={styles.card}>
-      <img src='src\assets\walid.jpg' className={styles.card__avatar} />
+      <img src='./assets/walid.jpg' className={styles.card__avatar} />
       <div className={styles.card__body}>
         <h3 className={styles.card__title}>Mohamad Qamhieh</h3>
         <p className={styles.card__subtitle}>Frontend Engineer</p>
